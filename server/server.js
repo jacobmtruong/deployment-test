159d90adf7bb9b94100c702051eb72fd834eea90
Jacob Truong <jacob.truong@ymail.com>
2022-12-05 20:44:46 -0800

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8000;
+const port = process.env.port || 5000;
 
 
 require('./config/mongoose.config')
